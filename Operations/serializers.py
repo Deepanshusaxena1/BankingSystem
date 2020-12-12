4554b498ebc955cdfd2c3f5b2860bf1f8aaac179
Deepanshu Saxena <saxenad765@gmail.com>
2020-12-12 15:11:57 +0530

    Added Account & Customer Serializers

---
+++ Operations/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from Operations.models import Customer, Account
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'
+
