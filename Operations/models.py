a36711a234d37a68dfca06d46a287fd1bc48fe1c
Deepanshu Saxena <saxenad765@gmail.com>
2020-12-12 13:39:23 +0530

    Added Account to models

--- Operations/models.py
+++ Operations/models.py
@@ -15,3 +15,20 @@ class Customer(models.Model):
     def __str__(self):
         return str(self.id) + " - " + str(self.customer_first_name)
 
+class Account(models.Model):
+    account_type_choices = (
+        ("savings", "savings"),
+        ("salary", "salary"),
+        ("loan", "loan"),
+        ("current", "current"),
+    )
+    account_number = models.CharField(max_length=15, unique=True)
+    account_holder = models.OneToOneField(Customer, on_delete=models.CASCADE)
+    current_amount = models.FloatField(default=0)
+    account_type = models.CharField(choices=account_type_choices, max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.account_number
+
